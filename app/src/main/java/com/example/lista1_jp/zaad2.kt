a47d1420529f3070aa2d2bff88c13b99836846ca
Maria Cicirko <268386@student.pwr.edu.pl>
2025-04-04 22:37:23 +0200

    Zakonczenie zadanie 2

--- app/src/main/java/com/example/lista1_jp/zaad2.kt
+++ app/src/main/java/com/example/lista1_jp/zaad2.kt
@@ -14,7 +14,6 @@ import kotlin.collections.MutableList
  *
  * Sources:
  * https://pl.wikipedia.org/wiki/Multizbi%C3%B3r - definicja multizbioru
- *
  */
 
 
@@ -23,33 +22,61 @@ import kotlin.collections.MutableList
  *
  * @param x [IntList] - multizbior typu listy liczb calkowitych
  * @param y [IntList] - multizbior typu listy liczb calkowitych
- *
  * @return [MutableList<Int>] czesc wspolna multizbiorow
  */
 fun wspolne(x: IntList, y: IntList): MutableList<Int> {
     val newList : MutableList<Int> = mutableListOf()
     val yToRemove : MutableList<Int> = mutableListOf()
-
-    for(i in 0 until y.size){
-        yToRemove.add(y[i]) // kopia y do usuwania wystapien
-    }
-    for (i in x.indices) {
-        if (yToRemove.contains(x[i])) {
-            newList.add(x[i])
-            yToRemove.remove(x[i])
+    if(x.isNotEmpty() && y.isNotEmpty()) {
+        for (i in 0 until y.size) {
+            yToRemove.add(y[i]) // kopia y do usuwania wystapien
         }
+        for (i in x.indices) {
+            if (yToRemove.contains(x[i])) {
+                newList.add(x[i])
+                yToRemove.remove(x[i])
+            }
+        }
+    }else{
+        throw IllegalArgumentException("Podane listy sa puste," +
+                " nie ma czesci wspolnej")
     }
     return newList
 }
 
+
+/**
+ * Funkcja testujaca funkcje wspolne
+ *
+ * @return ilosc niepoprawnych przejsc testow
+ */
+fun testWspolne(): Int{
+    var counter = 0
+    try {
+        val x = intListOf()
+        val y = intListOf()
+        val sumXY = wspolne(x, y)
+        println("Czesc wspolna multizbioru $x i multizbioru $y" +
+                " wynosi $sumXY")
+        counter++
+    }catch(e: IllegalArgumentException){
+        println("Test rzuca wyjatek - poprawne")
+    }
+    try {
+        val x = intListOf(2, 4, 8 ,5, 5, 2)
+        val y = intListOf(1, 3, 4, -3, 5, 7, 2, 2)
+        val sumXY = wspolne(x, y)
+        assert(sumXY == intListOf(2, 4, 5, 2)) {
+            counter++
+        }
+    }catch (e:AssertionError){
+        println("Test rzuca wyjatek - niepoprawne")
+    }
+    return counter
+}
+// brakuje weryfikacji czy to multizbior czyli czy jakis element sie powtarza
+
 fun main(){
-    val x = intListOf(2, 4, 8 ,5, 5, 2)
-    val y = intListOf(1, 3, 4, -3, 5, 7, 2, 2)
-
-    val sumXY = wspolne(x, y)
-    println("Czesc wspolna multizbioru $x i multizbioru $y" +
-            " wynosi $sumXY")
-    // [1, 2, 4, 5, 5], [1, 5, 9, 0, 3, 3]
-
-
+    val count = testWspolne()
+    println("Ilosc niepoprawnych testow: $count")
 }
